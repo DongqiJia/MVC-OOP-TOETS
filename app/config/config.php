ab937b81d2c193dcdafcf60f381d4f06391cf7d8
DongqiJia <95073122+DongqiJia@users.noreply.github.com>
2022-10-17 11:19:44 +0200

    bezig met mvc toets

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+   // Database constanten
+   define('DB_HOST', 'localhost');
+   define('DB_USER', 'root');
+   define('DB_PASS', '');
+   define('DB_NAME', 'mvc-oop-toets');
+
+   // Het pad naar de app map
+   define('APPROOT', dirname(dirname(__FILE__)));
+
+   // De URL van het de site
+   define('URLROOT', 'http://www.mvc-oop-toets.com');
+
+   // Naam van de site
+   define('SITENAME', 'MVC-OOP-TOETS');
+?>
---
+++ app/controllers/Landingpages.php
@@ -0,0 +1,13 @@
+<?php
+
+class Landingpages extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+    public function index()
+    {
+        echo '<h2>Dit is de mvc-oop-toets homepage</h2>';
+    }
+}
